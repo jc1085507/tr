1c2e01de1a535fb7e3a81eb0d38b23dc398eb570
jc1085507 <116060632+jc1085507@users.noreply.github.com>
2022-12-10 19:28:37 +0800

    1

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,15 @@
 FROM alpine:edge
 
-RUN apk update && \
-    apk add --no-cache ca-certificates caddy tor wget && \
-    wget -qO- https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | busybox unzip - && \
-    chmod +x /xray && \
-    rm -rf /var/cache/apk/*
+WORKAPP /app
 
-ADD start.sh /start.sh
+COPY . .
+
+RUN apk update && \
+    apk add --no-cache ca-certificates caddy wget && \
+    wget -qO- https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip | busybox unzip - && \
+    chmod +x /app/trojan-go /app/start.sh && \
+    rm -rf /var/cache/apk/* 
+
 RUN chmod +x /start.sh
 
-CMD /start.sh
+ENTRYPOINT [/app/start.sh]
--- start.sh
+++ start.sh
@@ -3,15 +3,12 @@
 # configs
 AUUID=24b4b1e1-7a89-45f6-858c-242cf53b5bdb
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
-CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
-ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/StoreFiles
+CONFIGCADDY=https://raw.githubusercontent.com/jc1085507/tr/main/etc/Caddyfile
+StoreFiles=https://raw.githubusercontent.com/jc1085507/tr/main/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -21,9 +18,7 @@ for file in $(ls /usr/share/caddy/$AUUID); do
     [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
 done
 
-# start
-tor &
 
-/xray -config /xray.json &
+/app/trojan-go -config /app/etc/config.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
